b75083d68e4d6d69b55c8ac64cce3623f2ef19fb
Marcin Lisowski <mnlisows@gmail.com>
2025-11-26 21:11:53 +0100

    feat(core): implementacja klasy Position do śledzenia lokalizacji w kodzie

---
+++ src/Position.py
@@ -0,0 +1,20 @@
+class Position:
+    def __init__(self, line, column):
+        self.line = line
+        self.column = column
+
+    def copy(self):
+        p = Position(self.line, self.column)
+        return p
+
+    def __str__(self):
+        return "Linia: " + str(self.line) + ", Kolumna: " + str(self.column)
+
+    def __eq__(self, other):
+        if not isinstance(other, Position):
+            return False
+        
+        if self.line == other.line and self.column == other.column:
+            return True
+        else:
+            return False
