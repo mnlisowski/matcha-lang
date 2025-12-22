d7495659e18d238cff1470de3d6ac83f55cb6dab
Marcin Lisowski <mnlisows@gmail.com>
2025-12-22 23:40:56 +0100

    refactor(lekser): nowa struktura drzewa

--- src/ast_nodes.py
+++ src/ast_nodes.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 from typing import List, Optional, Any
-from src.token import Token 
+
+@dataclass
+class SourceLocation:
+    line: int
+    column: int
+    
+    def __repr__(self):
+        return f"(L:{self.line}, C:{self.column})"
 
 class ASTNode:
     pass
@@ -8,102 +15,74 @@ class ASTNode:
 class Statement(ASTNode):
     pass
 
-
-@dataclass
-class Program(ASTNode):
-    statements: list[Statement]
-    
-
 class Expression(ASTNode):
     pass
 
-# statementy
+#wyrażenia
+@dataclass
+class AddExpression(Expression):
+    left: Expression
+    right: Expression
+    location: SourceLocation
+
+@dataclass
+class SubtractExpression(Expression):
+    left: Expression
+    right: Expression
+    location: SourceLocation
+
+@dataclass
+class MultiplyExpression(Expression):
+    left: Expression
+    right: Expression
+    location: SourceLocation
+
+@dataclass
+class DivideExpression(Expression):
+    left: Expression
+    right: Expression
+    location: SourceLocation
+
+@dataclass
+class Literal(Expression):
+    value: Any        
+    type_name: str
+    location: SourceLocation
+
+@dataclass
+class Variable(Expression):
+    name: str
+    location: SourceLocation
+
+#statementy
+
 @dataclass
 class Block(Statement):
     statements: List[Statement]
-
-
-@dataclass
-class AssignmentStatement(Statement):
-    target_name: str
-    expression: Expression
-
-@dataclass
-class ReturnStatement(Statement):
-
-    expression: Optional[Expression]
+    location: SourceLocation
 
 @dataclass
 class IfStatement(Statement):
     condition: Expression
     then_branch: Statement
-    else_branch: Optional[Statement] = None
+    else_branch: Optional[Statement]
+    location: SourceLocation
 
 @dataclass
 class WhileStatement(Statement):
     condition: Expression
     body: Statement
+    location: SourceLocation
 
 @dataclass
 class FunctionDefinition(Statement):
     name: str
-    params: List[str]
-    body: Statement
+    params: List[str]     
+    body: Block          
+    location: SourceLocation
+
 
 @dataclass
-class FunctionCallStatement(Statement):
-    name: str
-    arguments: List[Expression]
-
-@dataclass
-class MatchStatement(Statement):
-    pass
-
-
-# wyrażenia
-
-@dataclass
-class BinaryExpression(Expression):
-    left: Expression
-    operator: Token
-    right: Expression
-
-@dataclass
-class UnaryExpression(Expression):
-    operator: Token
-    operand: Expression
-
-@dataclass
-class Literal(Expression):
-    value: Any
-    type_name: str  
-
-@dataclass
-class Variable(Expression):
-    name: str
-
-@dataclass
-class FunctionCall(Expression):
-    name: str
-    arguments: List[Expression]
-
-
-# class MatchCaseCondition(ASTNode):
-#     pass
-
-# @dataclass
-# class DefaultMatchCondition(MatchCaseCondition):
-#     pass
-
-# @dataclass
-# class ExprMatchCondition(MatchCaseCondition):
-#     pass
-
-# @dataclass
-# class PatternMatchCondition(MatchCaseCondition):
-#     pass
-
-# @dataclass
-# class CaseBranch(ASTNode):
-#     pass
-
+class Program(ASTNode):
+    functions: List[FunctionDefinition]
+    location: SourceLocation
